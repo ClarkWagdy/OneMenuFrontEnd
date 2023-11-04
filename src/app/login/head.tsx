efb1389352d79870ad9ded24b9e71ea158b2afa0
clark <clarkwagdy1@gmail.com>
2023-11-04 17:15:54 +0200

    Add meta

--- src/app/login/head.tsx
+++ src/app/login/head.tsx
@@ -7,6 +7,8 @@ export default function Head() {
        
       <title>{strings.signin}</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
+      <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests" />
+
       <meta name="description" content="All in one chip" />
       <meta name="keywords" content="One card, NFC, nfc, chip" />
       <link rel="icon" href="/One.svg" />
