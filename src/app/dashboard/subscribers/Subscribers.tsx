b770651545fcabcb14fa5739e09112c9b4e2786f
clark <clarkwagdy1@gmail.com>
2023-12-03 01:39:42 +0200

    done

--- src/app/dashboard/subscribers/Subscribers.tsx
+++ src/app/dashboard/subscribers/Subscribers.tsx
@@ -86,7 +86,7 @@ export default function Subscribers() {
             Setdone(true);
             setTimeout(() => {
                 Setwrite(false)
-            }, 3000);
+            }, 1000);
         }
     }
     return (
