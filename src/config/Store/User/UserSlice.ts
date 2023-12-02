9e4730b68c3cd81fb2df61180db11e6102293b11
clark <clarkwagdy1@gmail.com>
2023-12-02 20:54:29 +0200

    done2

--- src/config/Store/User/UserSlice.ts
+++ src/config/Store/User/UserSlice.ts
@@ -2,21 +2,15 @@
 
 import { createSlice } from '@reduxjs/toolkit'
 import { UserT } from './UserType';
-
-
-
-const user: string = localStorage.getItem('User') as string;
 var initialState: UserT = {};
 if (typeof window !== 'undefined') {
+    const user: string = localStorage.getItem('User') as string;
     initialState = JSON.parse(user) as UserT;
 }
 export const UserSlicer = createSlice({
     name: "User",
     initialState,
     reducers: {
-
-
-
         SetUser(_state, action) {
             localStorage.clear();
             localStorage.setItem("User", JSON.stringify(action.payload));
