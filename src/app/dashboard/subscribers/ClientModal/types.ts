00352b48f7e1642a6d2dc85d9e4ecf6c5406f6be
ClarkWagdy <clarkwagdy1@gmail.com>
2025-08-31 15:58:30 +0300

    solve

--- src/app/dashboard/subscribers/ClientModal/types.ts
+++ src/app/dashboard/subscribers/ClientModal/types.ts
@@ -3,7 +3,9 @@ export interface RestaurantDataDTO {
   name: string;
   logo: string;
   color: string;
-
+ offerStatus: Boolean,
+    videoStatus: Boolean,
+    isActive: Boolean,
   ownerName: string;
   ownerPhoneNumber: string;
   ownerEmail: string;
